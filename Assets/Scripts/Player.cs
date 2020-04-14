df026b0fbbca730c45dfabdb7a1e4a5a43ffb687
Ahouston429 <ahouston@student.neumont.edu>
2020-04-14 13:38:30 -0600

    Camera and base player
    The script for the camera and the beginings of the player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int currentHealth = 0;
+    [SerializeField] int maxHealth = 0;
+    [SerializeField] float speed;
+    [SerializeField] Rigidbody2D RB = null;
+    Vector2 momentum;
+    PlayerActions action;
+    float actionTimeLeft;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            action.type = PlayerActions.Action.Jump;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+            action.type = PlayerActions.Action.Move;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, GetComponent<Rigidbody2D>().velocity.y);
+            action.type = PlayerActions.Action.Move;
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            action.type = PlayerActions.Action.Attack;
+        }
+
+        if(GetComponent<Rigidbody2D>().velocity == Vector2.zero)
+        {
+            action.type = PlayerActions.Action.None;
+        }
+    }
+}
