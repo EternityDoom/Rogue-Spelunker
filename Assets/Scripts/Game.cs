e131848d8122191d5c014f84eb2a766e2d0a8fcb
EternityDoom <EternityDoom@users.noreply.github.com>
2020-04-17 17:13:07 -0600

    Enemy update
    Updates the AI class to Enemy. Also updates Game to acquire its child tiles and enemies on awake for its lists.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,13 +6,17 @@ public class Game : MonoBehaviour
 {
     [SerializeField] GameObject playerPrefab = null;
     public Player player { get; set; }
-    //public List<Enemy> enemies { get; set; }
+    public List<Enemy> enemies { get; set; }
     public List<Tile> tiles { get; set; }
     public List<Powerup> powerups { get; set; }
     public bool[] powerFound { get; set; } = null;
     public Vector2 respawnPoint { get; set; } = new Vector2(0, 0);
 
-
+    private void Awake()
+    {
+        enemies.AddRange(gameObject.GetComponentsInChildren<Enemy>());
+        tiles.AddRange(gameObject.GetComponentsInChildren<Tile>());
+    }
 
     // Update is called once per frame
     void Update()
