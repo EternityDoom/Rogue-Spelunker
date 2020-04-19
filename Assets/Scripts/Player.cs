20a2b9fbf7db528ac7f8223af2a40674ae7c7d7a
Ahouston429 <ahouston@student.neumont.edu>
2020-04-19 13:53:39 -0600

    Player update
    created a temp base for the first area of the game. The player has been modifed so that it can now have powerups that are activated on pick up. powerups work and can be picked up and alter the world/player abilities.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,33 +8,49 @@ public class Player : MonoBehaviour
     [SerializeField] int maxHealth = 0;
     [SerializeField] float speed;
     [SerializeField] Rigidbody2D RB = null;
-    Vector3 movement;
+    //Vector3 movement;
     float actionTimeLeft;
-    float jumpForce = 4.0f;
+    float jumpForce = 6.0f;
+
+    Dictionary<int, bool> powerUps = new Dictionary<int, bool>();
+    //for power ups, create a dictonary with the key being the int value of the power up, the value is a bool, when a power up is collected, it will search the dictonary 
+    //and find the appropraite power up and turn it to true, these will be used in deteming the players actions 
+
+    //power ups
+    //1 - torch (begin progress & attack?)
+    //2 - double jump
+    //3 - 
+    //4 - pickaxe (reach end the game)
+
+    
 
     public void Start()
     {
         maxHealth = 100;
         currentHealth = maxHealth;
+        powerUps.Add(1, false);
+        powerUps.Add(2, false);
+        powerUps.Add(3, false);
+        powerUps.Add(4, false);
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && actionTimeLeft == 0)
+        if (Input.GetKeyDown(KeyCode.Space) && actionTimeLeft <= 0)
         {
-            RB.AddRelativeForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            actionTimeLeft = 2.0f;
+            RB.AddRelativeForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            actionTimeLeft = 1.0f;
         }
 
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * speed;
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * speed * Time.deltaTime;
 
         if (Input.GetMouseButtonDown(1) && actionTimeLeft == 0)
         {
-            actionTimeLeft = 3.0f;
+            actionTimeLeft = 2.0f;
         }
 
-        if(actionTimeLeft > 0.0f)
+        if(actionTimeLeft >= 0.0f)
         {
             actionTimeLeft -= Time.deltaTime;
         }
@@ -44,15 +60,34 @@ public class Player : MonoBehaviour
             Destroy(gameObject);
         }
 
+        if (powerUps[1] == true)
+        {
+            GameObject[] gos = GameObject.FindGameObjectsWithTag("firstWall");
+            foreach(GameObject go in gos)
+            {
+                go.GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+
     }
 
-    public void OnCollisionEnter(UnityEngine.Collision collision)
+    public void OnCollisionEnter2D(UnityEngine.Collision2D collision)
     {
         if(collision.gameObject.tag == "Enemy")
         {
             currentHealth -= 10;
             Destroy(collision.gameObject);
         }
+
+        if(collision.gameObject.tag == "powerUp")
+        {
+            Debug.Log("Picked up power up");
+            int index = collision.gameObject.GetComponent<Powerup>().Pickup();
+            powerUps[index] = true;
+            Destroy(collision.gameObject);
+        }
+
+
     }
 
 }
